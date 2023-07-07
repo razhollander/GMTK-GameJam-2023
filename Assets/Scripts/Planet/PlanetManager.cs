2c48c087ab2e6e946f7738b1fdd6aa2553b8e5f9
vladimir.sazonov <vova.sazonovvv@gmail.com>
2023-07-08 02:22:39 +0300

    add: planet parts

---
+++ Assets/Scripts/Planet/PlanetManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace Planet
+{
+    public class PlanetManager : MonoBehaviour
+    {
+        [SerializeField] private PlanetPart _planetPart;
+
+        public PlanetManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            CreateParts();
+        }
+
+        private void CreateParts()
+        {
+            var parts = GetComponentsInChildren<PartPositionGetter>();
+
+            foreach (var part in parts)
+            {
+                Instantiate(_planetPart, part.Position, Quaternion.LookRotation(part.Position));
+            }
+        }
+    }
+}
