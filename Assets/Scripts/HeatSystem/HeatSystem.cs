0e342985af8be36f375eae4cefd440ca6505f2d6
razhollander <50023823+razhollander@users.noreply.github.com>
2023-07-08 00:58:05 +0300

    create heat system

---
+++ Assets/Scripts/HeatSystem/HeatSystem.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class HeatSystem : MonoBehaviour
+{
+    public static HeatSystem Instance;
+    [SerializeField] private int _checkHeatIntervalInSeconds = 5;
+    private int _currentHeat = 0;
+    public int CurrentHeat
+    {
+        get
+        {
+            return _currentHeat;
+        }
+        private set
+        {
+            _currentHeat = Math.Clamp(value, 0, 100);
+        }
+        
+    }
+    private List<IHeatProvider> _heatProviders = new ();
+    private List<IHeatIntervalObserver> _heatIntervalObservers =  new ();
+        
+    public void AddHeatProvider(IHeatProvider heatProvider)
+    {
+        _heatProviders.Add(heatProvider);
+    }
+    
+    public void RemoveHeatProvider(IHeatProvider heatProvider)
+    {
+        _heatProviders.Remove(heatProvider);
+    }
+    
+    public void AddHeatIntervalObserver(IHeatIntervalObserver heatIntervalObserver)
+    {
+        _heatIntervalObservers.Add(heatIntervalObserver);
+    }
+    
+    public void RemoveHeatIntervalObserver(IHeatIntervalObserver heatIntervalObserver)
+    {
+        _heatIntervalObservers.Remove(heatIntervalObserver);
+    }
+
+    
+    public void Awake()
+    {
+        Instance = this;
+        CheckHeatInterval();
+    }
+
+    private UniTask CheckHeatInterval()
+    {
+        while (true)
+        {
+            UniTask.Delay(_checkHeatIntervalInSeconds * 100);
+            
+            var heatTotalDelta = 0;
+            _heatProviders.ForEach(x =>
+            {
+                heatTotalDelta += x.HeatProvided;
+                x.OnHeatInterval();
+            });
+
+            CurrentHeat += heatTotalDelta;
+            _heatIntervalObservers.ForEach(x => x.OnHeatInterval(CurrentHeat, heatTotalDelta));
+        }
+    }
+}
