db314667ff010d1dc17461a87b00d7ede9bf210b
razhollander <50023823+razhollander@users.noreply.github.com>
2023-07-09 14:13:10 +0300

    ui

--- Assets/Scripts/UI/Score.cs
+++ Assets/Scripts/UI/Score.cs
@@ -24,7 +24,7 @@ public class Score : MonoBehaviour
     void Update()
     {
         _score += Time.deltaTime * _scoreSpeed;
-        _text.text = ScoreAmount.ToString();
+        _text.text = "Score: "+ScoreAmount.ToString();
     }
 
     public void SaveHighScore()
