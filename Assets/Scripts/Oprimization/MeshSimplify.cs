85f5c1e5f933515ff846e458eb001f4f82d355db
vladimir.sazonov <vova.sazonovvv@gmail.com>
2023-07-09 10:49:39 +0300

    f

--- Assets/Scripts/Oprimization/MeshSimplify.cs
+++ Assets/Scripts/Oprimization/MeshSimplify.cs
@@ -6,9 +6,9 @@ using System.Collections;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEditor;
-using UnityEngine;
 using UnityEngine.Serialization;
 #endif
+using UnityEngine;
 
 namespace Oprimization
 {
