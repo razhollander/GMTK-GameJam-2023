abd261df45267880d42a5abd161e14875f957c7f
vladimir.sazonov <vova.sazonovvv@gmail.com>
2023-07-09 14:06:33 +0300

    f

--- Assets/Scripts/Planet/PlanetExplodeManager.cs
+++ Assets/Scripts/Planet/PlanetExplodeManager.cs
@@ -5,6 +5,8 @@ namespace Planet
     public class PlanetExplodeManager : MonoBehaviour
     {
         [SerializeField] private GameObject _explode;
+        [SerializeField] private ParticleSystem _particle;
+        
         public static PlanetExplodeManager Instance { get; private set; }
 
         private void Awake()
@@ -20,6 +22,7 @@ namespace Planet
             GameManager.Instance.Loose += () =>
             {
                 _explode.SetActive(true);
+                _particle.Play();
                 PlanetManager.Instance.gameObject.SetActive(false);
             };
         }
