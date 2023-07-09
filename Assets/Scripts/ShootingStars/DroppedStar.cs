eff7e07e603e29ae531d0fe0aa3424f6295fce10
razhollander <50023823+razhollander@users.noreply.github.com>
2023-07-09 19:01:50 +0300

    fixes

--- Assets/Scripts/ShootingStars/DroppedStar.cs
+++ Assets/Scripts/ShootingStars/DroppedStar.cs
@@ -1,12 +1,19 @@
 using Audio;
 using UnityEngine;
-
+using DG.Tweening;
 public class DroppedStar : MonoBehaviour
 {
     [SerializeField] private float timeToSelfDestruct = 5;
-    
+    [SerializeField] private GameObject _arrow;
+    [SerializeField] private float _arrowY;
     private void Start()
     {
+        if (_arrow != null)
+        {
+            transform.LookAt(Vector3.zero);
+            _arrow.transform.DOLocalMoveZ(_arrow.transform.localPosition.z + _arrowY, timeToSelfDestruct * 0.2f).SetLoops(-1, LoopType.Yoyo);
+        }
+
         Destroy(gameObject, timeToSelfDestruct);
     }
     
