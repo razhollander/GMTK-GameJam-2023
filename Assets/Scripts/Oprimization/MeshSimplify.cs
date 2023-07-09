1f5395b690d06521eb99bbc90053ee25ab93affd
vladimir.sazonov <vova.sazonovvv@gmail.com>
2023-07-09 10:44:36 +0300

    d

---
+++ Assets/Scripts/Oprimization/MeshSimplify.cs
@@ -0,0 +1,33 @@
+
+#if UNITY_EDITOR
+
+using System.Collections;
+
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+#endif
+
+namespace Oprimization
+{
+    public class MeshSimplify : MonoBehaviour
+    {
+        public float quality = 0.5f;
+
+#if UNITY_EDITOR
+        
+        [Button]
+        private void Simplify()
+        {
+            var originalMesh = GetComponent<MeshFilter>().sharedMesh;
+            var meshSimplifier = new UnityMeshSimplifier.MeshSimplifier();
+            meshSimplifier.Initialize(originalMesh);
+            meshSimplifier.SimplifyMesh(quality);
+            var destMesh = meshSimplifier.ToMesh();
+            GetComponent<MeshFilter>().sharedMesh = destMesh;
+        }
+#endif
+    }
+}
