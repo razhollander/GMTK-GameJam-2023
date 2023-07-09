500b202db9bf4d936e6e8435c82cb06548eb213e
vladimir.sazonov <vova.sazonovvv@gmail.com>
2023-07-09 09:27:20 +0300

    f

--- Assets/Scripts/Elements/TornadoElement.cs
+++ Assets/Scripts/Elements/TornadoElement.cs
@@ -1,4 +1,6 @@
+using Cysharp.Threading.Tasks;
 using Planet;
+using UniRx;
 using UnityEngine;
 
 namespace Elements
@@ -13,10 +15,11 @@ namespace Elements
         [SerializeField] private Transform renderer;
         [SerializeField] private float speed = 1;
         [SerializeField] private float radius = 5;
+        [SerializeField] private float _aliveSeconds = 10;
         private Vector3 wayUp;
         private Vector3 center = Vector3.zero;
         private Vector3 _lastPos;
-
+        private float _secondsTimer;
 
         private Land _firstLand;
         private Land target;
@@ -25,7 +28,8 @@ namespace Elements
         {
             _firstLand = land;
             var counterWhile = 0;
-            while (target == null || target.Vertex != 6 && counterWhile < 15)
+            target = null;
+            while ((target == null || target == land || target.Vertex != 6) && counterWhile < 15)
             {
                 counterWhile++;
                 var randomIndex = Random.Range(0, land.Neighbors.Count);
@@ -36,17 +40,32 @@ namespace Elements
             {
                 target = PlanetManager.Instance.Lands.Find(i => i.Vertex == 6);
             }
+
+            HitAsync();
         }
 
         private void Update()
         {
+            _secondsTimer += Time.deltaTime;
             var ray = new Ray(rayPos.position, -transform.up);
             Physics.Raycast(ray, out var hit, 1, world);
             wayUp = hit.normal;
-            // var newPos = hit.point;
-            // newPos += transform.up * yOffset;
-            // renderer.position = newPos;
             if (target) MoveTowardsTarget(target);
+
+            if ((transform.position - target.Position).magnitude < Epsilon)
+            {
+                UpdateTarget();
+            }
+
+            if (_secondsTimer > _aliveSeconds)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void UpdateTarget()
+        {
+            SetFirstLand(target);
         }
 
         private void MoveTowardsTarget(Land target)
@@ -57,20 +76,23 @@ namespace Elements
             var newDirection = direction + cross;
             direction.Normalize();
             if (newDirection.magnitude < 0.1) return;
-            
+
             var myTransform = transform;
             var pos = myTransform.position; //get position
             pos += newDirection * (speed * Time.deltaTime); //move forward
             var v = pos - center; //get new position relative to center of sphere
             pos = center + v.normalized * radius; //constrain position to surface of sphere
             myTransform.position = pos; //set position
-            
+
             if (myTransform.position - _lastPos != Vector3.zero)
             {
                 myTransform.rotation = Quaternion.FromToRotation(transform.up, myTransform.position) * transform.rotation;
             }
         }
-        
-        
+
+        private async UniTask HitAsync()
+        {
+            
+        }
     }
 }
